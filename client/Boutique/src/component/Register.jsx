efed3259baac82b151f2a583e359bfa1c8663e53
Miro <badyss.blt@gmail.com>
2023-07-21 15:08:33 +0200

    2e commit

--- client/Boutique/src/component/Register.jsx
+++ client/Boutique/src/component/Register.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import '../styles/Log.css'
-const Log = () => {
+import React from 'react';
 
+const Register = () => {
     return (
+        <div className='mainForm'>
             <div className='contentForm'>
-                <h1 className='titleForm'>Se connecter</h1>
+                <h1 className='titleForm'>S'inscrire</h1>
+                <h1 className='btn'>Se connecter</h1>
                 <form className='form'>
                     <input type="email" name="" placeholder='Email' className='btn'/>
                     <input type="password" name="" placeholder='Mot de passe' className='btn'/>
-                    <button type="submit" className='btn'>Se connecter</button>
+                    <button type="submit" className='btn'>S'inscrire</button>
                 </form>
             </div>
+        </div>
     );
 };
 
-export default Log;
+export default Register;
--- client/Boutique/src/pages/Profile.jsx
+++ client/Boutique/src/pages/Profile.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../component/Navbar';
 import Log from '../component/Log'
+import Register from '../component/Register';
 
 const Profile = () => {
+
+    const [signUpModal, setSignUpModal] = useState(true)
+    const [signInModal, setSignInModal] = useState(false)
+
+    const handleModals = (e) => {
+        if(e.target.id === 'register') {
+            setSignUpModal(true)
+            setSignInModal(false)
+        }else if(e.target.id === 'login'){
+            setSignUpModal(false)
+            setSignInModal(true)
+        }
+    }
+
     return (
         <div className='profil-page'>
             <Navbar/>
             <div className="log-container">
-                <Log/>
+            <div className='sideForm'>
+                <h1 className='titleSideForm'>Bon retour !</h1>
+                <div className='registerDiv'>
+                 <a onClick={handleModals} id='register'className='registerbtn'>S'inscrire</a>
+                 <a onClick={handleModals} id='login'className='registerbtn'>Se connecter</a>
+            </div>
+            </div>
+            </div>
+            <div>
+            {signUpModal && <Register/>}
+            {signInModal && <Log/>}
             </div>
             
         </div>
